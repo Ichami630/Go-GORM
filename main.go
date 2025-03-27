6028126add87877ad304e35f071e830d77af912b
ichami630 <brandonichami@gmail.com>
2025-03-27 16:00:12 +0100

    connecting to postgres db

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // load the env file
@@ -16,8 +18,54 @@ func loadEnv() {
 	}
 }
 
+// construct the postgres connection string
+func getConnectionString() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSLMODE"),
+	)
+}
+
+// type user struct {
+// 	gorm.Model
+// 	name  string
+// 	email string
+// }
+
+// type channel struct {
+// 	gorm.Model
+// 	name        string
+// 	description string
+// }
+
+// type message struct {
+// 	gorm.Model
+// 	content   string
+// 	userID    uint
+// 	channedID uint
+// 	user      user
+// 	channel   channel
+// }
+
+// perform the migrations
+// func setup(db *gorm.DB) {
+// 	db.AutoMigrate(&user{}, &channel{}, &message{})
+// }
+
 func main() {
 	loadEnv()
-	fmt.Println(os.Getenv("DB_USER"))
+
+	dns := getConnectionString()
+	_, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+
+	log.Println("Connected to the database successfully!")
 
 }
